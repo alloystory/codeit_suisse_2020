3650e55f5fad4a876d542669d6c1e8350a5056a1
Aloysius Chan <alloystory@gmail.com>
2020-09-26 10:50:31 +0800

    edit port number

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
+import os
 from src.app import app
 
 if __name__ == "__main__":
-    app.run(host = "127.0.0.1", port = 3000)
+    host = "127.0.0.1"
+    port = 3000 if not "PORT" in os.environ else os.environ.get("PORT")
+
+    app.run(host = host, port = port)
