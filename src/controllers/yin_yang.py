825442535eb2fcdd73ab8af293392e938463b02e
Aloysius Chan <alloystory@gmail.com>
2020-09-27 22:32:04 +0800

    edit yinyang

--- src/controllers/yin_yang.py
+++ src/controllers/yin_yang.py
@@ -1,35 +1,38 @@
 from flask import jsonify
 
-memoize = dict()
 def solver(n, k, E):
-    if k == 0:
-        return 0
-    else:
-        num_yang = 0
-        possible_orderings = set()
-        for x in range(n):
-            if E[x] == "Y" or E[n - x - 1] == "Y":
-                num_yang += 1
-            if E[x] == "Y":
-                possible_orderings.add(E[:x] + E[(x + 1):])
-            elif E[n - x - 1] == "Y":
-                possible_orderings.add(E[:(n - x - 1)] + E[(n - x):])
-            else:
-                possible_orderings.add(E[:x] + E[(x + 1):])
-                possible_orderings.add(E[:(n - x - 1)] + E[(n - x):])
+    memoize = dict()
+    def inner(n, k, E):
+        if k == 0:
+            return 0
+        else:
+            num_yang = 0
+            possible_orderings = set()
+            for x in range(n):
+                if E[x] == "Y" or E[n - x - 1] == "Y":
+                    num_yang += 1
+                if E[x] == "Y":
+                    possible_orderings.add(E[:x] + E[(x + 1):])
+                elif E[n - x - 1] == "Y":
+                    possible_orderings.add(E[:(n - x - 1)] + E[(n - x):])
+                else:
+                    possible_orderings.add(E[:x] + E[(x + 1):])
+                    possible_orderings.add(E[:(n - x - 1)] + E[(n - x):])
 
-        probability = num_yang / n
+            probability = num_yang / n
 
-        prob_possible_ordering = 0
-        for order in possible_orderings:
-            if order in memoize:
-                prob = memoize[order]
-            else:
-                prob = solver(n - 1, k - 1, order)
-                memoize[order] = prob
-            prob_possible_ordering += (prob / len(possible_orderings))
+            prob_possible_ordering = 0
+            for order in possible_orderings:
+                if order in memoize:
+                    prob = memoize[order]
+                else:
+                    prob = inner(n - 1, k - 1, order)
+                    memoize[order] = prob
+                prob_possible_ordering += (prob / len(possible_orderings))
 
-        return probability + prob_possible_ordering
+            return probability + prob_possible_ordering
+    output = inner(n, k, E)
+    return output
 
 def solve(data):
     n = data["number_of_elements"]
