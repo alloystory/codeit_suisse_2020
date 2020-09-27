649d39ff17dc30f4294a515b19345130dfceea44
Aloysius Chan <alloystory@gmail.com>
2020-09-27 11:37:42 +0800

    edit clean floor

--- src/controllers/clean_floor.py
+++ src/controllers/clean_floor.py
@@ -4,22 +4,28 @@ def all_clean(arr):
     return all(map(lambda x: x == 0, arr))
 
 def calculate(arr):
-    print("Data:", arr)
-    i = 0
     num_moves = 0
-    while i < (len(arr) - 1):
-        i += 1
+    for i in range(1, len(arr)):
         num_moves += 1
-        arr[i] = not arr[i]
+        if arr[i] == 0:
+            arr[i] = 1
+        else:
+            arr[i] -= 1
 
-        if arr[i - 1] == 1:
+        if arr[i - 1] != 0:
+            num_moves += arr[i - 1] * 2
             arr[i - 1] = 0
-            num_moves += 1
+            arr[i] -= arr[i - 1]
+            if arr[i] % 2 == 0:
+                arr[i] = 0
+            else:
+                arr[i] = 1
             if all_clean(arr):
-                break
-            num_moves += 1
-        elif all_clean(arr):
+                num_moves -= 1
+        
+        if all_clean(arr):
             break
+
     return num_moves
         
 def solve(data):
